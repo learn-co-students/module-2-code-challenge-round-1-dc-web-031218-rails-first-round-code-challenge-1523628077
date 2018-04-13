cb01e2b8071ecb481b97ba05683f8d325b5ace9c
Austin Wolfe <wolfe_austin@yahoo.com>
2018-04-13 11:14:41 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :superpowers, only: [:index]
-  resources :superheroes, only: [:index]
-
+  resources :superpowers, only: [:index, :show]
+  resources :superheroes, only: [:index, :show, :create, :new]
+  # get '/search', to: 'superhero#search', as :show
 end
