23fa8a1d266c3146a81a0a8f47a8e35c6c788cab
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-13 11:19:37 -0400

    .

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,52 @@
 class SuperheroesController < ApplicationController
+  before_action :set_superhero, only: [:show, :edit, :update, :destroy]
+
   def index
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
     @superheroes = Superhero.all
   end
+
+  def show
+    #set_superhero
+  end
+
+  def edit
+    #set_superhero
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+
+  def update
+    #set_superhero
+  end
+
+  def create
+    @new_hero = Superhero.new(superhero_params)
+
+    if @new_hero.valid?
+      @new_hero.save
+      redirect_to superheroes_path
+    else
+      # @new_hero = Superhero.new(superhero_params) tried adding this here but didn't work
+      @errors = true
+      render :new
+    end
+  end
+
+  def destroy
+    #set_superhero
+  end
+
+  private
+    def superhero_params
+      params.require(:superhero).permit(:name, :super_name, :superpower_id)
+    end
+
+    def set_superhero
+      @superhero = Superhero.find(params[:id])
+    end
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,8 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  # Make sure no two superheroes have the same super name.
+  validates :super_name, uniqueness: true
+  validates :super_name, presence: true
+  validates :name, presence: true
+
 end
